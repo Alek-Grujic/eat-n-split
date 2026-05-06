7b2921616aa4fb453375325180d75e5340f65ccb
Alek-Grujic <alek.grujic001@outlook.com>
2026-05-06 02:57:55 +0200

    feat: implement friend selection logic

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,10 @@ function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <Button>Add friend</Button>
+        <FormAddFriend />
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -60,8 +63,51 @@ function Friend({ friend }) {
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>Friend name</label>
+      <input type="text" />
+
+      <label>Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-add-friend">
+      <h2>Split a bill with </h2>
+
+      <label>💰Bill value</label>
+      <input type="text" />
+
+      <label>💹Your expense</label>
+      <input type="text" />
+
+      <label>👮‍♀️X</label>
+      <input type="text" disabled />
+
+      <label>🤑Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
   );
 }
 
